777db0e5424b4f5db94728cabcdc35c0458d34f0
Kwan Leong Chit Jeff <informaverse@gmail.com>
2025-06-07 19:23:55 +0800

    inner scale bug

--- model/Harmonics.py
+++ model/Harmonics.py
@@ -43,25 +43,26 @@ class HyperEdgeAttention(nn.Module):
                                          dropout=dropout)
         self.z_norm = nn.LayerNorm(channels)
 
-    def _compute_edges(self, tokens: torch.Tensor) -> torch.Tensor:
+    def _compute_edges(self, tokens, scale):
         w = self.hyperedge(tokens)
-        w = F.relu(w) * F.softmax(w, dim=-1) * (self.edges/S1/S2/S3)
+        w = F.relu(w) * F.softmax(w, dim=-1) * scale
         w = self.dropout(w)
         z = self.z_norm(w.transpose(1, 2) @ tokens)
         return z
 
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
+    def forward(self, x):
         # x: (B, C, S1, S2, S3)
         B, C, S1, S2, S3 = x.shape
         tokens = x.permute(0, 2, 3, 4, 1).reshape(B, S1 * S2 * S3, C)
-        tokens = self.in_norm(tokens)                                    # (B, N, C)
+        tokens = self.in_norm(tokens)
 
         if self.training and x.requires_grad:
-            z = checkpoint.checkpoint(self._compute_edges, tokens, use_reentrant=False)
+            scale =  self.edges / S1 / S2 / S3
+            z = checkpoint.checkpoint(self._compute_edges, tokens, scale, use_reentrant=False)
         else:
-            z = self._compute_edges(tokens)                              # (B, E, C)
+            z = self._compute_edges(tokens, scale)
             
-        y = self.mha(tokens, z, z, need_weights=False)[0]                # (B, N, C)
+        y = self.mha(tokens, z, z, need_weights=False)[0]
 
         y = y.permute(0, 2, 1).reshape(B, C, S1, S2, S3)
         return y
