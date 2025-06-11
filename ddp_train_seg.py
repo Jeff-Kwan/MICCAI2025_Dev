2145c4ce1b4870dd54fc444d3fa6626962ded944
Kwan Leong Chit Jeff <informaverse@gmail.com>
2025-06-11 17:23:07 +0800

    sync ddp

--- ddp_train_seg.py
+++ ddp_train_seg.py
@@ -79,7 +79,7 @@ def main_worker(rank: int,
         train_ds,
         batch_size=train_params['batch_size'],
         sampler=train_sampler,
-        num_workers=24,
+        num_workers=32,
         prefetch_factor=1,
         pin_memory=True,
         persistent_workers=True)
