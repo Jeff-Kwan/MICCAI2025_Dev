3e107f8ec48d97b285ceb116b2b4ab2f03e05716
Kwan Leong Chit Jeff <informaverse@gmail.com>
2025-06-16 10:26:11 +0800

    just spatial crop

--- utils/dataset.py
+++ utils/dataset.py
@@ -17,10 +17,9 @@ def get_transforms(shape, num_crops, spatial, intensity, coarse):
                 keys=["image", "label"], 
                 dtype=[torch.float32, torch.long],
                 track_meta=False),
-            mt.RandSpatialCropSamplesd( # Does not support on GPU
+            mt.RandSpatialCropd( # Does not support on GPU
                 keys=["image", "label"], 
                 roi_size=shape,
-                num_samples=num_crops,
                 lazy=True),
             mt.DivisiblePadd(
                 keys=["image", "label"],
