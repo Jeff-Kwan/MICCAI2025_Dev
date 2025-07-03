605d67baa473de4d4e34886b20f4c24a14d1bc55
Kwan Leong Chit Jeff <informaverse@gmail.com>
2025-07-03 15:29:02 +0800

    soft - trilinear spacing the mean ensemble

--- data_prep/preprocess2softlabel.py
+++ data_prep/preprocess2softlabel.py
@@ -4,7 +4,6 @@ from tqdm import tqdm
 import torch
 import monai.transforms as mt
 from monai.data import Dataset, ThreadDataLoader
-
 from monai.config import KeysCollection
 
 class NormalizeChannelSumd(mt.MapTransform):
@@ -19,7 +18,7 @@ class NormalizeChannelSumd(mt.MapTransform):
 
     def __call__(self, data):
         d = dict(data)
-        for key in self.key_iterator(d):
+        for key in self.keys:
             img = d[key]
             # Compute sum across channels at each voxel location
             sum_across_channels = img.sum(dim=0, keepdim=True)
@@ -37,6 +36,7 @@ def get_pseudo_data(aladdin, blackbean, extension=".nii.gz"):
     for img_path in aladdin.glob(f"*{extension}"):
         aladdin_path = aladdin / img_path.name
         blackbean_path = blackbean / img_path.name
+        assert blackbean_path.exists(), f"Match for {img_path.name} not found in {blackbean}"
         data.append({
             "aladdin": str(aladdin_path),
             "blackbean": str(blackbean_path)
@@ -56,8 +56,7 @@ def process_dataset(aladdin, blackbean, out_dir, pixdim):
             mt.EnsureTyped(
                 keys=["aladdin", "blackbean"],
                 dtype=torch.long,
-                track_meta=True,
-            ),
+                track_meta=True),
             mt.ThresholdIntensityd(
                 keys=["aladdin", "blackbean"],
                 above=False,
@@ -65,25 +64,23 @@ def process_dataset(aladdin, blackbean, out_dir, pixdim):
                 cval=0),
             mt.AsDiscreted(
                 keys=["aladdin", "blackbean"],
-                to_onehot=True),
+                to_onehot=14),  # 14 classes
             mt.EnsureTyped(
                 keys=["aladdin", "blackbean"],
                 dtype=torch.float32,
                 track_meta=True),
-            mt.Orientationd(keys=["aladdin", "blackbean"], axcodes="RAS", lazy=True),
-            mt.Spacingd(
-                keys=["aladdin", "blackbean"],
-                pixdim=pixdim,
-                mode= "trilinear",
-                lazy=True,
-            ),
             mt.MeanEnsembled(
                 keys=["aladdin", "blackbean"],
-                output_key="label",
-                lazy=True,
-            ),
+                output_key="label"),
             mt.DeleteItemsd(
                 keys=["aladdin", "blackbean"]),
+            mt.Orientationd(keys=["label"], axcodes="RAS", lazy=True),
+            mt.Spacingd(
+                keys=["label"],
+                pixdim=pixdim,
+                mode="trilinear",
+                lazy=True,
+            ),
             NormalizeChannelSumd(keys=["label"]),
             mt.SaveImaged(
                 keys=["label"],
@@ -91,8 +88,9 @@ def process_dataset(aladdin, blackbean, out_dir, pixdim):
                 output_postfix="",
                 output_ext=".nii.gz",
                 separate_folder=False,
-                output_dtype=torch.uint8,
-                print_log=False)
+                output_dtype=torch.float32,
+                print_log=False),
+            mt.DeleteItemsd(keys=["label"])
         ]
     )
 
@@ -101,7 +99,7 @@ def process_dataset(aladdin, blackbean, out_dir, pixdim):
     dataloader = ThreadDataLoader(
         dataset,
         batch_size=1,
-        num_workers=160,
+        num_workers=24,
     )
 
     # iterate, transform, and save
@@ -116,7 +114,7 @@ if __name__ == "__main__":
     pixdim = (0.8, 0.8, 2.5)
     process_dataset(
         "data/FLARE-Task2-LaptopSeg/train_pseudo_label/flare22_aladdin5_pseudo",
-        "data/FLARE-Task2-LaptopSeg/train_pseudo_label/pseudo_label_blackbean_flare22"
+        "data/FLARE-Task2-LaptopSeg/train_pseudo_label/pseudo_label_blackbean_flare22",
         "data/nifti/train_pseudo/softlabel",
         pixdim)
     
