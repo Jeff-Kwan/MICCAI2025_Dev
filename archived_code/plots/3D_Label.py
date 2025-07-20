19fbbb6c191518514146c624a1f4bc6a28e9dec6
Kwan Leong Chit Jeff <informaverse@gmail.com>
2025-07-20 14:34:12 +0800

    plots code

--- archived_code/plots/3D_Label.py
+++ archived_code/plots/3D_Label.py
@@ -1,18 +1,18 @@
 import monai.transforms as mt
 import napari
-import torch
 
 # Load your two label volumes
 loader = mt.Compose([
     mt.LoadImaged(keys=["vol1", "vol2"], ensure_channel_first=True),
-    mt.CropForegroundd(keys=["vol1", "vol2"], source_key="vol2"),
-    mt.KeepLargestConnectedComponentd(keys=["vol1"]),
+    # mt.CropForegroundd(keys=["vol1", "vol2"], source_key="vol2"),
+    # mt.KeepLargestConnectedComponentd(keys=["vol1"]),
     mt.Orientationd(keys=["vol1", "vol2"], axcodes="RAS"),
     mt.Spacingd(keys=["vol1", "vol2"], pixdim=(1.5, 1.5, 1.5), mode=['nearest', 'nearest']),
 ])
+num = '42'
 data = loader({
-    "vol1": r"archived_code\plots\labels\val_outputs\FLARETs_0031.nii.gz",
-    "vol2": r"archived_code\plots\labels\Validation-Public-Labels\FLARETs_0031.nii.gz"
+    "vol1": rf"archived_code\plots\labels\attnunet3_output\FLARETs_00{num}.nii.gz",
+    "vol2": rf"archived_code\plots\labels\Validation-Public-Labels\FLARETs_00{num}.nii.gz"
 })
 vol1 = data["vol1"].numpy().astype(int)
 vol2 = data["vol2"].numpy().astype(int)
@@ -23,22 +23,22 @@ viewer = napari.Viewer(ndisplay=3)
 # Prediction
 # viewer.add_labels(
 #     vol1,
-#     name="Seg A",
-#     opacity=0.4,
+#     name="Prediction",
+#     opacity=0.2,
 # )
 
 # Label
 viewer.add_labels(
     vol2,
-    name="Seg B",
-    opacity=0.8,
+    name="Ground Truth",
+    opacity=0.2,
 )
 
-# Difference mask, binary (0=transparent, 1=magenta)
+# Difference mask, binary
 viewer.add_labels(
     diff,
     name="Differences",
-    opacity=0.4,
+    opacity=0.8,
 )
 
 napari.run()
--- archived_code/rank_dice.py
+++ archived_code/rank_dice.py
@@ -1,12 +1,12 @@
-import torch
 from monai.metrics import DiceMetric
 import monai.transforms as mt
 import os
 import glob
 import numpy as np
 from tqdm import tqdm
+from multiprocessing import Pool
 
-val_pred_dir = "archived_code/plots/labels/val_outputs"
+val_pred_dir = "archived_code/plots/labels/attnunet3_output"
 val_labels_dir = "archived_code/plots/labels/Validation-Public-Labels"
 
 pred_files = glob.glob(os.path.join(val_pred_dir, "*.nii.gz"))
@@ -15,19 +15,17 @@ val_files = glob.glob(os.path.join(val_labels_dir, "*.nii.gz"))
 pred_map = {os.path.basename(f): f for f in pred_files}
 val_map = {os.path.basename(f): f for f in val_files}
 
-common_names = set(pred_map.keys()) & set(val_map.keys())
+common_names = list(set(pred_map.keys()) & set(val_map.keys()))
 
-results = []
-dice_metric = DiceMetric(include_background=False, ignore_empty=False)
 loader = mt.Compose([
     mt.LoadImaged(keys=["vol1", "vol2"], ensure_channel_first=True),
-    mt.CropForegroundd(keys=["vol1", "vol2"], source_key="vol2"),
-    mt.KeepLargestConnectedComponentd(keys=["vol1"]),
+    # mt.CropForegroundd(keys=["vol1", "vol2"], source_key="vol2", margin=64, allow_smaller=True),
+    # mt.KeepLargestConnectedComponentd(keys=["vol1"]),
     mt.AsDiscreted(keys=["vol1", "vol2"], to_onehot=14),
-    mt.EnsureTyped(keys=["vol1", "vol2"], dtype=[torch.long, torch.long], track_meta=False),
 ])
 
-for name in tqdm(common_names, desc="Calculating Dice"):
+def compute_dice(name):
+    dice_metric = DiceMetric(include_background=False, ignore_empty=False)
     dice_metric.reset()
     data = loader({
         "vol1": pred_map[name],
@@ -35,11 +33,16 @@ for name in tqdm(common_names, desc="Calculating Dice"):
     })
     pred = data["vol1"].unsqueeze(0)
     label = data["vol2"].unsqueeze(0)
-    results.append((name, dice_metric(pred, label).cpu().numpy()))
+    dice = dice_metric(pred, label).cpu().numpy()
+    return (name, dice)
 
-results.sort(key=lambda x: x[1].mean())
+if __name__ == "__main__":
+    with Pool(2) as pool:
+        results = list(tqdm(pool.imap(compute_dice, common_names), total=len(common_names), desc="Calculating Dice"))
 
-for name, dice in results:
-    print(f"{name}: {np.round(dice, 4)}")
+    results.sort(key=lambda x: x[1].mean())
 
-print(f"\nMean Dice: {sum(d[1].mean() for d in results) / len(results):.4f}")
+    for name, dice in results:
+        print(f"{name}: {np.round(dice, 4)}")
+
+    print(f"\nMean Dice: {sum(d[1].mean() for d in results) / len(results):.4f}")
