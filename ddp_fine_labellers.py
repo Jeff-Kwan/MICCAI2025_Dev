e65e85ef2e7ed541647837b58af3e31c32ff8598
Kwan Leong Chit Jeff <informaverse@gmail.com>
2025-07-23 21:58:27 +0800

    56 workers

--- ddp_fine_labellers.py
+++ ddp_fine_labellers.py
@@ -105,7 +105,7 @@ def main_worker(rank: int,
             train_ds,
             batch_size=train_params['batch_size'],
             sampler=train_sampler,
-            num_workers=44,
+            num_workers=56,
             prefetch_factor=2,
             pin_memory=False,
             persistent_workers=True)
