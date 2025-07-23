2059f24fffe139ef732207c16b2fae31b0c6c18c
Kwan Leong Chit Jeff <informaverse@gmail.com>
2025-07-23 23:25:12 +0800

    high prefetch

--- ddp_fine_labellers.py
+++ ddp_fine_labellers.py
@@ -105,8 +105,8 @@ def main_worker(rank: int,
             train_ds,
             batch_size=train_params['batch_size'],
             sampler=train_sampler,
-            num_workers=36,
-            prefetch_factor=4,
+            num_workers=32,
+            prefetch_factor=16,
             pin_memory=False,
             persistent_workers=True)
         val_loader = DataLoader(
