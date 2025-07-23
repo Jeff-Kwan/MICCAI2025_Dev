a8914af633c21a584b58f0ccd3224d306bd9e5cc
Kwan Leong Chit Jeff <informaverse@gmail.com>
2025-07-23 21:03:52 +0800

    attnunet4 training

--- ddp_fine_labellers.py
+++ ddp_fine_labellers.py
@@ -105,8 +105,8 @@ def main_worker(rank: int,
             train_ds,
             batch_size=train_params['batch_size'],
             sampler=train_sampler,
-            num_workers=46,
-            prefetch_factor=2,
+            num_workers=32,
+            prefetch_factor=3,
             pin_memory=False,
             persistent_workers=True)
         val_loader = ThreadDataLoader(
