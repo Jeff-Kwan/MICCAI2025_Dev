0f0cf38d53191f0770782b5a2c131d02d4016a7a
Kwan Leong Chit Jeff <informaverse@gmail.com>
2025-07-26 00:37:36 +0800

    main loop

--- run_code.py
+++ run_code.py
@@ -1,4 +1,5 @@
 import subprocess
 
-subprocess.run(["python3", "ddp_fine_labellers.py"])
-subprocess.run(["python3", "inference.py"])
+if __name__ == "__main__":
+    subprocess.run(["python3", "ddp_fine_labellers.py"])
+    subprocess.run(["python3", "inference.py"])
