98d814213bda3f9b6e1fe111e0fda1b285c4eb00
Kwan Leong Chit Jeff <informaverse@gmail.com>
2025-07-28 18:07:01 +0800

    sync

--- inference.py
+++ inference.py
@@ -56,7 +56,7 @@ def get_pre_transforms(pixdim, intensities):
 def get_post_transforms(pre_transforms, out_dir):
     return mt.Compose([
         mt.AsDiscreted(keys="pred", argmax=True),
-        # RemoveSmallObjectsPerClassd(keys=["pred"]),
+        RemoveSmallObjectsPerClassd(keys=["pred"]),
         mt.Invertd(
             keys="pred",
             transform=pre_transforms,
