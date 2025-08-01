ff0b63772faf2f23fb31bc5aecdbdb7585d6bd60
Kwan Leong Chit Jeff <informaverse@gmail.com>
2025-08-01 21:13:06 +0800

    43 workers

--- ddp_fine_1x.py
+++ ddp_fine_1x.py
@@ -99,7 +99,7 @@ def main_worker(rank: int,
             train_ds,
             batch_size=train_params['batch_size'],
             sampler=train_sampler,
-            num_workers=42,
+            num_workers=43,
             prefetch_factor=4,
             pin_memory=False,
             persistent_workers=True)
