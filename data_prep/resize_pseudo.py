178e322aa48e324efb5250c00045233a18549cda
Kwan Leong Chit Jeff <informaverse@gmail.com>
2025-08-02 21:52:57 +0800

    fewer workers resize pseudo

--- data_prep/resize_pseudo.py
+++ data_prep/resize_pseudo.py
@@ -101,8 +101,8 @@ def process_pseudo(datafiles, output_dir):
     dataloader = ThreadDataLoader(
         Dataset(data=datafiles, transform=transform),
         batch_size=1,
-        num_workers=42,
-        prefetch_factor=16,
+        num_workers=38,
+        prefetch_factor=32,
         pin_memory=False,
         persistent_workers=True)
     
