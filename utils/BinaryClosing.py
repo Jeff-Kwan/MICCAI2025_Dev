6214e72ee1e5e32db69a3528ce5199d6b5bcaea4
Kwan Leong Chit Jeff <informaverse@gmail.com>
2025-08-03 09:00:46 +0800

    sync

--- utils/BinaryClosing.py
+++ utils/BinaryClosing.py
@@ -1,5 +1,5 @@
 from typing import Hashable, Any, Mapping
-from skimage.morphology import binary_closing, ball
+from skimage.morphology import binary_closing
 import numpy as np
 from monai.config import KeysCollection
 from monai.transforms import MapTransform, KeepLargestConnectedComponent
@@ -12,20 +12,20 @@ class BinaryClosingd(MapTransform):
     def __init__(
         self,
         keys: KeysCollection,
-        radius: int = 4,
+        width: int = 4,
         classes: int = 14,
         allow_missing_keys: bool = False,
     ):
         super().__init__(keys, allow_missing_keys=allow_missing_keys)
-        self.radius = radius
+        self.width = width
         self.classes = classes
-        self.footprint = ball(radius)
+        self.footprint = np.ones((width, width, width), dtype=bool)  # Cubic footprint
 
     def __call__(self, data: Mapping[Hashable, Any]) -> dict:
         for key in self.key_iterator(data):
             for class_index in range(1, self.classes):
                 fg = (data[key] == class_index).squeeze(0)
-                fg = binary_closing(fg, footprint=self.footprint).bool()
+                fg = binary_closing(fg, footprint=self.footprint)
                 fg = np.expand_dims(fg, axis=0)
                 data[key][(fg & (data[key] == 0))] = class_index    # Extensive
         return data
@@ -36,21 +36,21 @@ class BinaryClosingForegroundd(MapTransform):
     def __init__(
         self,
         keys: KeysCollection,
-        radius: int = 4,
+        width: int = 4,
         connectivity: int = 3,
         allow_missing_keys: bool = False):
         super().__init__(keys, allow_missing_keys=allow_missing_keys)
-        self.radius = radius
+        self.width = width
         self.connectivity = connectivity
-        self.footprint = ball(self.radius)
+        self.footprint = np.ones((width, width, width), dtype=bool)  # Cubic footprint
         self.keeplargest = KeepLargestConnectedComponent(connectivity=3)
         
     def __call__(self, data: Mapping[Hashable, Any]) -> dict:
         for key in self.key_iterator(data):
             orig_fg = (data[key] > 0).squeeze(0)
-            closed = binary_closing(orig_fg, footprint=self.footprint).bool()
+            closed = binary_closing(orig_fg, footprint=self.footprint)
             closed = orig_fg | closed  # force extensivity explicitly
             closed = np.expand_dims(closed, 0)
-            closed = self.keeplargest(closed).bool()  # if you still want largest after that
+            closed = self.keeplargest(closed)
             data[key][~closed] = 0
         return data
