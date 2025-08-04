e1e58c3df35fc7f00da8f0227565307377f2aac1
Kwan Leong Chit Jeff <informaverse@gmail.com>
2025-08-04 12:08:06 +0800

    also compute nsd

--- archived_code/rank_dice.py
+++ archived_code/rank_dice.py
@@ -1,16 +1,14 @@
-from monai.metrics import DiceMetric
+from monai.metrics import DiceMetric, compute_surface_dice
 import monai.transforms as mt
 import os
 import glob
 import numpy as np
-import torch
 from tqdm import tqdm
 from multiprocessing import Pool
 import pandas as pd
 
 import os, sys
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-from utils.RemoveSmall import RemoveSmallObjectsPerClassd
 
 val_pred_dir = "archived_code/plots/labels/au6_official"
 val_labels_dir = "archived_code/plots/labels/Validation-Public-Labels"
@@ -23,37 +21,9 @@ val_map = {os.path.basename(f): f for f in val_files}
 
 common_names = list(set(pred_map.keys()) & set(val_map.keys()))
 
-from monai.data import MetaTensor
-from skimage.morphology import remove_small_objects
-class RemoveSmallObjectsPerClassd(mt.Transform):
-    def __init__(self, keys, labels, min_sizes, connectivity=1):
-        self.keys = keys
-        self.labels = labels
-        self.min_sizes = min_sizes
-        self.conn = connectivity
 
-    def __call__(self, data):
-        for key in self.keys:
-            img = data[key].cpu().numpy()
-            for lbl, ms in zip(self.labels, self.min_sizes):
-                mask = (img == lbl)
-                if mask.any():
-                    cleaned_mask = remove_small_objects(mask, min_size=ms, connectivity=self.conn)
-                    img[mask & (~cleaned_mask)] = 0
-            if isinstance(data[key], MetaTensor):
-                data[key] = MetaTensor(img, meta=data[key].meta)
-            elif isinstance(data[key], torch.Tensor):
-                data[key] = torch.tensor(img, dtype=data[key].dtype, device=data[key].device)
-            else:
-                data[key] = img
-        return data
-    
 loader = mt.Compose([
-    mt.LoadImaged(keys=["vol1", "vol2"], ensure_channel_first=True),
-    # mt.CropForegroundd(keys=["vol1", "vol2"], source_key="vol2", margin=64, allow_smaller=True),
-    # mt.KeepLargestConnectedComponentd(keys=["vol1"], independent=True, num_components=2),
-    # mt.RemoveSmallObjectsd(keys=["vol1"], min_size=256),
-    # RemoveSmallObjectsPerClassd(keys=["vol1"]),
+    mt.LoadImaged(keys=["vol1", "vol2"], ensure_channel_first=True, image_only=False),
     mt.AsDiscreted(keys=["vol1", "vol2"], to_onehot=14),
 ])
 ignore_empty = False
@@ -70,7 +40,25 @@ def compute_dice(name):
     dice = dice_metric(pred, label).cpu().numpy()
     return (name, dice)
 
+def compute_NSD(name):
+    data = loader({
+        "vol1": pred_map[name],
+        "vol2": val_map[name]
+    })
+    pred = data["vol1"].unsqueeze(0)
+    label = data["vol2"].unsqueeze(0)
+    nsd = compute_surface_dice(
+        y_pred=pred, y=label,
+        include_background=False,
+        class_thresholds=[1]*13,
+        spacing=data["vol1"].meta["pixdim"][1:4]
+    ).cpu().numpy()
+    return (name, nsd)
+
 if __name__ == "__main__":
+    result_name = "au6_official"
+
+    # Dice
     with Pool(50) as pool:
         results = list(tqdm(pool.imap(compute_dice, common_names), total=len(common_names), desc="Calculating Dice"))
     mean_dice = sum(np.nanmean(d[1]) for d in results) / len(results)
@@ -83,11 +71,21 @@ if __name__ == "__main__":
                                 "Aorta", "IVC", "RAG", "LAG",
                                 "Gallbladder", "Esophagus", "Stomach", "Duodenum", "L-Kidney"]
     )
-    print(df.to_string(index=False, col_space=8, justify="center"))
-    if ignore_empty:
-        suffix = "_ignore_empty"
-    else:
-        suffix = ""
-    df.to_csv(f"archived_code/au6_official.csv", index=False)
-
+    df.to_csv(f"archived_code/{result_name}_dice.csv", index=False)
     print(f"\nMean Dice: {mean_dice:.4f}")
+
+    # NSD
+    with Pool(50) as pool:
+        results = list(tqdm(pool.imap(compute_NSD, common_names), total=len(common_names), desc="Calculating NSD"))
+    mean_nsd = sum(np.nanmean(d[1]) for d in results) / len(results)
+    results.sort(key=lambda x: x[1].mean())
+    results = [[x[0].replace(".nii.gz", "")] + np.round(x[1].squeeze(), 3).tolist() for x in results]
+
+    df = pd.DataFrame(
+        results,
+        columns=["Name"] + ["Liver", "R-Kidney", "Spleen", "Pancreas", 
+                                "Aorta", "IVC", "RAG", "LAG",
+                                "Gallbladder", "Esophagus", "Stomach", "Duodenum", "L-Kidney"]
+    )
+    df.to_csv(f"archived_code/{result_name}_nsd.csv", index=False)
+    print(f"\nMean NSD: {mean_nsd:.4f}")
